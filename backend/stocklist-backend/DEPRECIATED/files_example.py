c68e2af9d75a4cd068504d49e4a042746c879d3b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 00:07:27 +0000

    Adding tarfile member sanitization to extractall()

--- backend/stocklist-backend/DEPRECIATED/files_example.py
+++ backend/stocklist-backend/DEPRECIATED/files_example.py
@@ -61,7 +61,26 @@ with SSHTunnelForwarder(
 
             with tarfile.open(mode="r:gz", fileobj=grid_out) as tar:
                 assert isinstance(tar, tarfile.TarFile)
-                tar.extractall(path="..")
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path="..")
             try:
                 subprocess.check_call(["make"], stdout=FNULL, stderr=FNULL)
             except subprocess.CalledProcessError:
